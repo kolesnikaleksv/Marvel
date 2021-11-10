0d3b983f91c7fcdf8aed9ce6a48bbf01d0830a47
Alex <kolesnikaleksv@gmail.com>
2021-11-10 13:23:12 +0200

    a couple the homework from the teacher

--- src/components/randomChar/RandomChar.js
+++ src/components/randomChar/RandomChar.js
@@ -27,6 +27,8 @@ class RandomChar extends Component {
     
     render() {
         const {char: {name, description, thumbnail, homepage, wiki}} = this.state;
+        const descr = description ? description : "No data about this character.";
+        const descr2 = descr.length > 210 ? descr.slice(0,210) + "..." : descr;
         return (
             <div className="randomchar">
                 <div className="randomchar__block">
@@ -34,7 +36,7 @@ class RandomChar extends Component {
                     <div className="randomchar__info">
                         <p className="randomchar__name">{name}</p>
                         <p className="randomchar__descr">
-                            {description}
+                            {descr2}
                         </p>
                         <div className="randomchar__btns">
                             <a href={homepage} className="button button__main">
