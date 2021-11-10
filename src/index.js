d269d481f636e4779a248cf23c79f216c5b18fd8
Alex <kolesnikaleksv@gmail.com>
2021-11-10 09:31:16 +0200

    Receiving data from the server

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/App';
-import MarvelService from './services/MarvelService';
 
 import './style/style.scss';
 
-const newMarvelService = new MarvelService();
-
-// newMarvelService.getAllCaracters().then(res => console.log(res.data.results));
-newMarvelService.getAllCaracters().then(res => res.data.results.forEach(item => console.log(item.name)));
-
-newMarvelService.getCaracter(1010338).then(res => console.log(res));
 
 
 ReactDOM.render(
